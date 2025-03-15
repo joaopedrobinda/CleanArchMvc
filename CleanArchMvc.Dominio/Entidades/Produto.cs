f2f572cd3fd20ab595040df01cddb45b0fa920ab
João Pedro Binda da Silva <bindadasilvajoaopedro@gmail.com>
2025-03-14 23:09:18 -0300

    ckeckpoint - entidades modelo anemico e gitignore

---
+++ CleanArchMvc.Dominio/Entidades/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Dominio.Entidades
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; private set; }
+        public string Descricao { get; private set; }
+        public decimal Preco { get; private set; }
+        public int Estoque { get; private set; }
+        public string Imagem { get; private set; }
+
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+    }
+}
