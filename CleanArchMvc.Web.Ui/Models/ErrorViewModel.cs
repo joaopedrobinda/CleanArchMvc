17e9237cb71ba2108d86abc5bfc4aa54b58722b3
João Pedro Binda da Silva <bindadasilvajoaopedro@gmail.com>
2025-03-14 21:52:57 -0300

    checkpoint base

---
+++ CleanArchMvc.Web.Ui/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CleanArchMvc.Web.Ui.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
