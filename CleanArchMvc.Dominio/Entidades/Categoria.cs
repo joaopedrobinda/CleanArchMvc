d028c8defa7b5ddf6fdbb328355e9888f023244f
João Pedro Binda da Silva <bindadasilvajoaopedro@gmail.com>
2025-03-15 00:40:12 -0300

    checkpoint - entidades enriquecidas

--- CleanArchMvc.Dominio/Entidades/Categoria.cs
+++ CleanArchMvc.Dominio/Entidades/Categoria.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CleanArchMvc.Dominio.Validador;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +7,34 @@ using System.Threading.Tasks;
 
 namespace CleanArchMvc.Dominio.Entidades
 {
-    public class Categoria
+    public sealed class Categoria : EntidadeBase
     {
-        public int Id { get; set; }
         public string Nome { get; private set; }
         public ICollection<Produto> Productos { get; private set; }
 
+        public Categoria(string nome)
+        {
+            ValidadorDominio(nome);
+        }
 
+        public void Update(string nome)
+        {
+            ValidadorDominio(nome);
+        }
+
+        public Categoria(int id, string nome)
+        {
+            ValidadorExcecaoDominio.Onde(id < 0, "Valor do Id inválido");
+            Id = id;
+            ValidadorDominio(nome);
+        }
+
+        private void ValidadorDominio (string nome)
+        {
+            ValidadorExcecaoDominio.Onde(string.IsNullOrEmpty(nome), "Nome invalido, nome é requerido");
+            ValidadorExcecaoDominio.Onde(nome.Length < 3, "Nome invalido, mínimo de 3 caracteres");
+
+            Nome = nome;
+        }
     }
 }
